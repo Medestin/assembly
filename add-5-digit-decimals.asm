2d014ad4d6ef6e44d91e00e52f8c5a3d5d486bd0
Medestin <laskawski.marcin@gmail.com>
2019-11-11 12:26:35 +0100

    add 5 digin numbers with aaa

---
+++ add-5-digit-decimals.asm
@@ -0,0 +1,41 @@
+section .data
+    msg db "The sum is: ", 0xa
+    len equ $ - msg
+    num1 db '12345'
+    num2 db '23456'
+    sum db '     '
+
+section .text
+    global _start
+
+_start:
+    mov esi, 4  ;pointing the rightmost digit
+    mov ecx, 5  ;num of digits
+    clc
+    
+add_loop:
+    mov al, [num1 + esi]
+    adc al, [num2 + esi]
+    aaa
+    pushf
+    or al, 30h
+    popf
+
+    mov [sum + esi], al
+    dec esi
+    loop add_loop
+
+    mov edx, len
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov edx, 5
+    mov ecx, sum
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov eax, 1
+    int 0x80
